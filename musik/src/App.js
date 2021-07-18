9855d42dc118ec4617ee1e1eb96876407f5779ae
heshanu <heshanu97@gmail.com>
2021-07-18 20:09:30 +0530

    error

--- musik/src/App.js
+++ musik/src/App.js
@@ -1,32 +1,32 @@
-//import logo from './logo.svg';
-import './App.css';
-import React,{Component} from 'react';
-class App extends Component{
-  state={artistQuery:''};
-  updateQuery=event=>{
-    console.log('event.target.value',event.target.value);
-    this.setState({artistQuery:event.target.value})
-  }
+// //import logo from './logo.svg';
+// import './App.css';
+// import React,{Component} from 'react';
+// class App extends Component{
+//   state={artistQuery:''};
+//   updateQuery=event=>{
+//     console.log('event.target.value',event.target.value);
+//     this.setState({artistQuery:event.target.value})
+//   }
 
-  handleKeyPress=(event)=>{
-    if(event.key === 'Enter'){
-      this.searchArtist();
-    }
-     // console.log('this.state',this.state);
-   }
-  searchArtist=()=>{
-    console.log('this.state',this.state);
-  } 
+//   handleKeyPress=(event)=>{
+//     if(event.key === 'Enter'){
+//       this.searchArtist();
+//     }
+//      // console.log('this.state',this.state);
+//    }
+//   searchArtist=()=>{
+//     console.log('this.state',this.state);
+//   } 
 
-  render(){
-    return(
-      <div class="search">
-        <h2>Hello,Welcome!</h2>
-        <input onChange={this.updateQuery} placeholder="Search for an artist? " onKeyPress={this.handleKeyPress}/>
-        <button onClick={this.searchArtist}>Search</button>
-      </div>
-    )
-  }
-}
+//   render(){
+//     return(
+//       <div class="search">
+//         <h2>Hello,Welcome!</h2>
+//         <input onChange={this.updateQuery} placeholder="Search for an artist? " onKeyPress={this.handleKeyPress}/>
+//         <button onClick={this.searchArtist}>Search</button>
+//       </div>
+//     )
+//   }
+// }
 
-export default App;
+// export default App;
